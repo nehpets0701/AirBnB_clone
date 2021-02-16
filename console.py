05ea29706baf47af482b0c6477a23c8d7d877451
nehpets <nehpets0701@gmail.com>
2021-02-16 03:57:56 +0000

    seven and nine complete but not tested

--- console.py
+++ console.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-import cmd
-
-
 """main console class with cmd module"""
+import cmd
+from models import storage
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.base_model import BaseModel
+
+classes = {"BaseModel": BaseModel, "State": State, "City": City,
+           "Amenity": Amenity, "Place": Place, "Review": Review, "User": User}
+
+
 class HBNBCommand(cmd.Cmd):
     """main console class with cmd module"""
     prompt = "(hbnb) "
@@ -16,26 +28,85 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """skeleton for create"""
-        pass
+        """creates a new instances of base model"""
+        if args in classes:
+            new = classes[args]()
+            new.save()
+            print(new.id)
+        elif (len(args) == 0):
+            print("** class name missing **")
+        else:
+            print("** class doesn't exist **")
 
     def do_show(self, args):
-        """skeleton for show"""
-        pass
+        """shows string representation of an instance"""
+        strList = args.split()
+        if (len(strList) == 0):
+            print("** class name missing **")
+        elif (len(strList) == 1):
+            print("** instance id missing **")
+        elif strList[0] in classes:
+            iid = "{}.{}".format(strList[0], strList[1])
+            dicti = storage.all().to_dict()
+            if iid in dicti.keys():
+                print(dicti[iid])
+            else:
+                print("** no instance found **")
 
     def do_destroy(self, args):
-        """skeleton for destroy"""
-        pass
+        """destroys an instance"""
+        strList = args.split()
+        if (len(strList) == 0):
+            print("** class name missing **")
+        elif (len(strList) == 1):
+            print("** instance id missing **")
+        elif strList[0] in classes:
+            iid = "{}.{}".format(strList[0], strList[1])
+            dicti = storage.all()
+            if iid in dicti.keys():
+                del dicti[iid]
+            else:
+                print("** no instance found **")
 
     def do_all(self, args):
-        """skeleton for all"""
-        pass
+        """prints string representation for all instances"""
+        strList = args.split()
+        if (len(strList) == 0):
+            print(storage.all())
+        elif (len(strList) == 1 and strList[0] in classes):
+            print(storage.all())
+            for x in storage.all():
+                for y in x:
+                    print(y)
+        else:
+            print("** class doesn't exist **")
 
     def do_update(self, args):
-        """skeleton for update"""
-        pass
-
-
+        """updates an instance"""
+        strList = args.split()
+        if (len(strList) == 0):
+            print("** class name missing **")
+        elif (len(strList) == 1):
+            print("** instance id missing **")
+        elif len(strList) == 2:
+            print("** attribute name missing **")
+        elif len(strList) == 3:
+            print("** value missing **")
+        elif strList[0] in classes:
+            iid = "{}.{}".format(strList[0], strList[1])
+            dicti = storage.all()
+            if iid in dicti.keys():
+                idict = dicti[iid]
+                if strList[2] in idict.keys() and strList[2]:
+                    idict[strList[2]] = strList[3]
+                    storage.save()
+                else:
+                    idict[strList[2]] = strList[3]
+                    storage.save()
+            else:
+                print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""review data"""
+
+
+class Review(BaseModel):
+    """review data"""
+    text = ""
+    user_id = ""
+    place_id = ""
