2e2acd9e7560dd91e0685329e172ef47e522caf5
mittsahl <msalem311@aol.com>
2021-02-16 15:30:47 +0000

    Working base model

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,24 @@ class BaseModel:
     
     def __init__(self, *args, **kwargs):
         """Instantion method"""
-        now = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = now
-        self.updated_at = now
-
+        if len(kwargs) != 0:
+            """Create instance"""
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    setattr(self, key, type(self))
+                elif key == 'created_at' or key == 'updated_at':
+                    """Formats datetime"""
+                    setattr(self, key, datetime.strptime(value,  "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            """No dict create new instance"""
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            from models.__init__ import storage
+            storage.new(self)
+    
     def __str__(self):
         """Overriding __str__ method"""
         retstr = "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -23,14 +36,16 @@ class BaseModel:
         """Updates updated at attribute"""
         now = datetime.now()
         self.updated_at = now
-
+        from models.__init__ import storage
+        storage.save()
+    
     def to_dict(self):
-        """Returns dict values"""
-        book = self.__dict__
-        book["__class__"] = self.__class__.__name__
-        tmp = book["created_at"]
-        book["created_at"] = tmp.isoformat()
-        tmp = book["updated_at"]
-        book["updated_at"] = tmp.isoformat()
-        return book
-
+        """Returns dictionary instance attributes"""
+        attributes = {}
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                attributes[key] = value.isoformat()
+            else:
+                attributes[key] = value
+        attributes['__class__'] = type(self).__name__
+        return attributes
