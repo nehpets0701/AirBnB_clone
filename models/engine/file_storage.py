584048ffafbd8bb15c595fd74cd83ba5a29fd5a8
mittsahl <msalem311@aol.com>
2021-02-16 15:31:29 +0000

    Working file storage script

---
+++ models/engine/file_storage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""File Storage engine"""
+
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    """File storage engine"""
+    __file_path = "file.json"
+    __objects = {}
+    
+    def all(self):
+        """Returns dict __objects"""
+        return self.__objects
+    
+    def new(self, obj):
+        """adds object to object dict with id key"""
+        self.__objects[str(type(obj).__name__) + "." + obj.id] = obj
+        
+    def save(self):
+        """Serializes objects to json file"""
+        thangs = []
+        for key, value in self.__objects.items():
+            thangs.append(value.to_dict())
+        with open(self.__file_path, "w+") as f:
+            f.write(json.dumps(thangs))
+    
+    def reload(self):
+        """Deserializes json file back into objects {} dict"""
+        try:
+            with open(self.__file_path, "r") as f:
+                leest = json.loads(f.read())
+            for obj in leest:
+                if obj['__class__'] == "BaseModel":
+                    self.new(BaseModel(**obj))
+        except:
+            pass
