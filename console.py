72806b0383bd628db1b68ad6818233ab87ae7f7f
nehpets <nehpets0701@gmail.com>
2021-02-17 20:56:08 +0000

    import basemodel and removed extra import

--- console.py
+++ console.py
@@ -10,7 +10,6 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models.base_model import BaseModel
 
 classes = {"BaseModel": BaseModel, "State": State, "City": City,
            "Amenity": Amenity, "Place": Place, "Review": Review, "User": User}
--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """state data"""
+from models.base_model import BaseModel
 
 
 class State(BaseModel):
